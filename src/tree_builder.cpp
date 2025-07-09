868271a81104f0cea2586d38914e93dc06239b08
Iván Delgado Ramos <93808733+Ivancitop@users.noreply.github.com>
2025-07-08 18:32:55 -0600

    Update tree_builder.cpp

--- src/tree_builder.cpp
+++ src/tree_builder.cpp
@@ -68,9 +68,8 @@ BT::ControlNode* BT::TreeBuilder::BuildTree()
     // Search ball fallback
     fallback_search_ball->AddChild(search_ball);
     fallback_search_ball->AddChild(turning_head_home_seq); */
-
-    //turning_head_home_seq->AddChild(walk_to_target);
     turning_head_home_seq->AddChild(simple_walk);
+    turning_head_home_seq->AddChild(walk_to_target);
     // Add sequences to fallback
     main_fallback->AddChild(turning_head_home_seq);
     main_fallback->AddChild(right_kick);
