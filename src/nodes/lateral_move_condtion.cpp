e1ac15866a5fb8178645406de21baf29ad12d89a
Iván Delgado Ramos <93808733+Ivancitop@users.noreply.github.com>
2025-07-08 22:44:10 -0600

    Create lateral_move_condtion.cpp

---
+++ src/nodes/lateral_move_condtion.cpp
@@ -0,0 +1,45 @@
+/*
+    Authors:
+        Pedro Deniz
+        Marlene Cobian
+*/
+
+#include "movement_pkg/nodes/lateral_move_condition.h"
+
+
+BT::LateralMoveCondition::LateralMoveCondition(const std::string &name) 
+: BT::ConditionNode(name) {
+   robot_state_sub_ = nh_.subscribe("/robotis_" + std::to_string(robot_id) + "/via_libre_state", 1, &LateralMoveCondition::robotStateCallback, this);
+}
+
+BT::ReturnStatus BT::LateralMoveCondition::Tick()
+{
+    // Condition checking and state update
+    while(ros::ok())
+    {        
+
+        if (current_robot_state_ >=1 && current_robot_state_ <=2)
+        {
+            ROS_SUCCESS_LOG("Robot get valid data to move lateral");
+            set_status(BT::SUCCESS);
+            return BT::SUCCESS;
+            
+        }
+        else
+        {
+            ROS_COLORED_LOG("Not receiving valid data", CYAN, false);
+            set_status(BT::FAILURE);
+            return BT::FAILURE;
+            
+        }
+    }
+    ROS_ERROR_LOG("ROS stopped unexpectedly", false);
+    return BT::FAILURE;
+}
+
+// Callback para recibir el estado del robot desde el tópico /via_libre_state
+void BT::vMoveCondition::robotStateCallback(const std_msgs::Int32::ConstPtr& msg)
+{
+    current_robot_state_ = msg->data;
+    ROS_INFO("WalkToTarget: Received robot state: %d", current_robot_state_);
+}
