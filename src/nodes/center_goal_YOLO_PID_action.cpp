5afc51c56846f4d6fa745557774f3afd85b61bef
pedrodeniz48 <pedrodeniz11@gmail.com>
2025-07-16 14:14:42 -0300

    compilation errors for goal msgs

--- src/nodes/center_goal_YOLO_PID_action.cpp
+++ src/nodes/center_goal_YOLO_PID_action.cpp
@@ -57,8 +57,8 @@ void BT::CenterGoalYOLOPID::WaitForTick()
             if (fabs(error_pan) < error_limit_ && fabs(error_tilt) < error_limit_)
             {
                 ROS_SUCCESS_LOG("Goal IN CENTER! Calculating init pose");
-                goal_msg_.distance = calculateDistance(head_tilt_angle_);
-                goal_msg_.angle = head_pan_angle_;
+                goal_msg_.distance.data = calculateDistance(head_tilt_angle_);
+                goal_msg_.angle.data = head_pan_angle_;
                 goal_fts_pub_.publish(goal_msg_)
                 set_status(BT::SUCCESS);
                 break;
