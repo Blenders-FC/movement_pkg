124b4e3ec682925bbd9948dd441a487736c7d5cb
vicman <giltafollavictormanuel@gmail.com>
2026-01-17 11:54:22 -0600

    ball detected condition

---
+++ include/movement_pkg/nodes/ball_detected_condition.h
@@ -0,0 +1,43 @@
+
+/*
+    Authors:
+        Pedro Deniz
+        Marlene Cobian
+        Victor Gil
+*/
+
+#ifndef BALL_DETECTED_CONDITION_H
+#define BALL_DETECTED_CONDITION_H
+
+#include "movement_pkg/utils.h"
+#include "movement_pkg/cb_data_manager.h"
+#include <behaviortree_cpp/bt_factory.h>
+#include "behaviortree_cpp/condition_node.h"
+#include <rclcpp/rclcpp.hpp>
+#include <chrono>
+#include <memory>
+
+
+namespace BT
+{
+class BallDetectedCondition : public BT::ConditionNode
+{
+    public:
+        explicit BallDetectedCondition(const std::string& name, const BT::NodeConfig& config);  // Constructor
+
+
+        ~BallDetectedCondition() override = default;
+
+        static BT::PortsList providedPorts();
+
+        // Behavior Tree Tick function
+        BT::NodeStatus tick() override;
+
+    private:
+        rclcpp::Node::SharedPtr node_;
+        geometry_msgs::msg::Point ball_center_position_;
+	CBDataManager cb_data_manager_;
+};
+}  // namesapce BT
+
+#endif  // BALL_DETECTED_CONDITION_H
