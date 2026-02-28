77007540d895cb0c6e313818cdea9354f3016749
vicman <giltafollavictormanuel@gmail.com>
2026-02-28 12:02:08 -0600

    send head to home reset

---
+++ include/movement_pkg/nodes/send_head_to_home_reset.h
@@ -0,0 +1,43 @@
+/*
+    Authors:
+        Pedro Deniz
+        Marlene Cobian
+*/
+
+#ifndef HEAD_TO_HOME_RESET_H
+#define HEAD_TO_HOME_RESET_H
+
+#include "movement_pkg/cb_data_manager.h"
+#include "behaviortree_cpp/action_node.h"
+#include <sensor_msgs/msg/joint_state.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+
+namespace BT
+{
+class HeadToHomeReset : public StatefulActionNode, public CBDataManager
+{
+    public:
+        // Constructor
+        explicit HeadToHomeReset(const std::string& name, const BT::NodeConfig& config);
+        ~HeadToHomeReset();
+
+        static BT::PortsList providedPorts();
+
+        NodeStatus onStart() override;
+        NodeStatus onRunning() override;
+        void onHalted() override;
+
+    private:
+        //  Auxiliar methods
+        void writeHeadJoint(double ang_value, bool is_pan);
+
+        // ROS variable
+        rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr write_joint_pub_;
+        sensor_msgs::msg::JointState write_msg_;
+
+        TargetInfo m_turncnt;
+};
+}  // namespace BT
+
+#endif  // HEAD_TO_HOME_ACTION_H
--- src/nodes/send_head_to_home_reset.cpp
+++ src/nodes/send_head_to_home_reset.cpp
@@ -6,36 +6,37 @@
         Victor Gil
 */
 
-#include "movement_pkg/nodes/send_head_to_home_action.h"
+#include "movement_pkg/nodes/send_head_to_home_reset.h"
+
 
 namespace BT {
-    HeadToHome::HeadToHome(
+    HeadToHomeReset::HeadToHomeReset(
         const std::string& name, 
         const BT::NodeConfig& config)
     : StatefulActionNode(name, config)
     {
-        node_ = rclcpp::Node::make_shared("send_head_to_home");
-        RCLCPP_INFO(node_->get_logger(), "[HeadToHome] constructed");
+        node_ = rclcpp::Node::make_shared("send_head_to_home_reset");
+        RCLCPP_INFO(node_->get_logger(), "[HeadToHomeReset] constructed");
         write_joint_pub_ = this->create_publisher<sensor_msgs::msg::JointState>("/robotis_" + std::to_string(robot_id) + "/direct_control/set_joint_states", 0);
     }
 
-BT::HeadToHome::~HeadToHome() {}
+BT::HeadToHomeReset::~HeadToHomeReset() {}
 
-NodeStatus HeadToHome::onStart()
+NodeStatus HeadToHomeReset::onStart()
 {
-    RCLCPP_INFO(node_->get_logger(), "[HeadToHome] START");
+    RCLCPP_INFO(node_->get_logger(), "[HeadToHomeReset] START");
 
     return NodeStatus::RUNNING;
 }
 
-NodeStatus BT::HeadToHome::onRunning()
+NodeStatus BT::HeadToHomeReset::onRunning()
 {
 
     // set_status(BT::RUNNING);
     setModule("direct_control_module");
     rclcpp::sleep_for(std::chrono::milliseconds(1000));
     //ROS_COLORED_LOG("Set Module to direct_control_module", YELLOW, false);
-    RCLCPP_INFO(node_->get_logger(), "[HeadToHome] Set Module to direct_control_module");
+    RCLCPP_INFO(node_->get_logger(), "[HeadToHomeReset] Set Module to direct_control_module");
 
     writeHeadJoint(0, true);
     writeHeadJoint(-10, false);
@@ -43,12 +44,15 @@ NodeStatus BT::HeadToHome::onRunning()
     RCLCPP_INFO(node_->get_logger(), "[HeadToHome] New tilt angle position from head2home: %d", -10);
     rclcpp::sleep_for(std::chrono::milliseconds(2000));
 
-    RCLCPP_INFO(node_->get_logger(), "[HeadToHome] Head in home position!");
+    //ROS_SUCCESS_LOG("Head in home position! Resetting Counter");
+    RCLCPP_INFO(node_->get_logger(), "[HeadToHome] Head in home position! Resetting Counter");
+    m_turncnt.turncnt = 0;
+    getBlackboard()->setTarget("m_turncnt", m_turncnt);
     return NodeStatus::SUCCESS;
 
 }
 
-void BT::HeadToHome::writeHeadJoint(double ang_value, bool is_pan)
+void BT::HeadToHomeReset::writeHeadJoint(double ang_value, bool is_pan)
 {
     write_msg_.header.stamp = node_->get_clock()->now(); 
         
@@ -68,13 +72,8 @@ void BT::HeadToHome::writeHeadJoint(double ang_value, bool is_pan)
     write_joint_pub_->publish(write_msg_);
 }
 
-BT::PortsList BT::HeadToHome::providedPorts()
+void BT::HeadToHomeReset::onHalted()
 {
-    return {};
+    RCLCPP_INFO(node_->get_logger(), "[HeadToHomeReset] HALTED: Stopped head movement");
 }
-
-void BT::HeadToHome::onHalted()
-{
-    RCLCPP_INFO(node_->get_logger(), "[HeadToHome] HALTED: Stopped head movement");
-}
-}
+}  // namespace BT
