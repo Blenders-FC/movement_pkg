4de820654ab154ebd3290e86dd3117e61bfc0ac7
RicardoBerumen <ricberar@gmail.com>
2026-03-21 14:19:23 -0600

    add stand up to flow

--- src/bt_plugin_register.cpp
+++ src/bt_plugin_register.cpp
@@ -11,4 +11,5 @@ extern "C" void BT_RegisterNodesFromPlugin(BT::BehaviorTreeFactory& factory)
     factory.registerNodeType<BT::ManagerRunningCondition>("ManagerRunning");
     factory.registerNodeType<BT::LeftKick>("LeftKick");
     factory.registerNodeType<BT::SimpleWalk>("SimpleWalk");
+    factory.registerNodeType<BT::StandUp>("StandUp");
 }
