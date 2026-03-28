ba1528e400c70f01f68acb3368578a19c6061a5d
vicman <giltafollavictormanuel@gmail.com>
2026-03-28 10:06:33 -0600

    manager done condition

---
+++ include/movement_pkg/nodes/manager_done_condition.h
@@ -0,0 +1,30 @@
+/*
+    Authors:
+        Pedro Deniz
+        Marlene Cobian
+*/
+
+#ifndef MANAGER_DONE_CONDITION_H
+#define MANAGER_DONE_CONDITION_H
+
+#include "movement_pkg/cb_data_manager.h"
+#include "behaviortree_cpp/condition_node.h"
+
+
+namespace BT
+{
+class ManagerDoneCondition : public ConditionNode, public CBDataManager
+{
+    public:
+        explicit ManagerDoneCondition(const std::string &name, const BT::NodeConfig& config);  // Constructor
+
+        // Behavior Tree Tick function
+        BT::NodeStatus tick() override;
+
+    private:
+        bool already_logged_ = false;
+        std::pair<std::string, std::string> robot_status_;
+};
+}  // namesapce BT
+
+#endif  // MANAGER_DONE_CONDITION_H
